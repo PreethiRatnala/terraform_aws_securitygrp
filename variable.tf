5e9e142ff6363326096c7fe73452191dc83bcc5b
PreethiRatnala <preethi.ratnala64@gmail.com>
2024-09-18 11:52:48 +0530

    terraform

--- variable.tf
+++ variable.tf
@@ -14,3 +14,33 @@ variable "sg_name" {
 variable "sg_description" {
   type = string
 }
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "common_tags" {
+  type = map
+}
+
+variable "sg_tags" {
+  type = map
+  default = {}
+}
+
+variable "outbound_rules" {
+  type = list
+  default = [
+    {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+variable "ingress_rules" {
+  type = list
+  default = []
+}
